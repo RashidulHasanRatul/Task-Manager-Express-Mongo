dfb68cf2a2df04090f92ea6629b7cc130ed81ca5
Rashidul Hasan <rhratul12@gmail.com>
2022-09-18 03:06:35 +0600

    add check_login middleware

--- src/middleware/check_login.js
+++ src/middleware/check_login.js
@@ -1,8 +1,24 @@
-const check_login = (req, res, next) => {
-  if (!req.session.user) {
-    res.redirect("/login");
-  } else {
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const check_login = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { _id } = decoded;
+    const user = await User.findOne({
+      _id: _id,
+      "tokens.token": token,
+    });
+    req.user = user;
+    req.username = user.name;
+    req.userId = _id;
+    req.token = token;
     next();
+  } catch (e) {
+    console.log(e);
+    next("Authentication Failed!!!!");
   }
 };
 
