e47b49916c5fec6e08698f451489012dd8550e99
Rashidul Hasan <rhratul12@gmail.com>
2022-09-18 03:08:12 +0600

    added task to  index file

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const dotenv = require("dotenv");
 const app = express();
 const port = process.env.PORT || 3000;
 const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
 dotenv.config();
-app.use(userRouter);
 app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
