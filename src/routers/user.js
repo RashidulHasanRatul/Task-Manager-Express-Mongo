c2cd4688cd0b2294c72c846fbe6dce4528aebbff
Rashidul Hasan <rhratul12@gmail.com>
2022-09-18 03:08:28 +0600

    added user router

---
+++ src/routers/user.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const router = new express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const check_login = require("../middleware/check_login");
+
+// signup
+router.post("/users", async (req, res) => {
+  try {
+    const oldUser = await User.findOne({ email: req.body.email });
+    if (oldUser) {
+      return res.status(400).send({ error: "Email already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 8);
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+    res.status(201).send("User created");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+// login
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).send("Unable to login");
+    }
+
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (isMatch) {
+      // generate token
+      const token = jwt.sign(
+        { _id: user._id.toString() },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "7 days",
+        }
+      );
+      user.tokens = user.tokens.concat({ token });
+      await user.save();
+
+      res.send({
+        token: token,
+        message: "Login successful",
+      });
+    } else {
+      return res.status(400).send("Unable to login");
+    }
+  } catch (e) {
+    res.status(400).send({ error: "Authentication Failed" });
+  }
+});
+
+// profile
+router.get("/users/profile", check_login, async (req, res) => {
+  res.send(req.user);
+});
+
+// logout
+router.post("/users/logout", check_login, async (req, res) => {
+  console.log(req.user);
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send("Logout successful");
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+});
+// logout all
+router.post("/users/logoutAll", check_login, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send("Logout ALL successful");
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+});
+
+// get all user
+router.get("/users", check_login, async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// get user by id
+router.get("/users/:id", check_login, async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// update user
+router.patch("/users/:id", check_login, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+  try {
+    const user = await User.findById(req.params.id);
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// delete user
+router.delete("/users/:id", check_login, async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// test route
+router.get("/test", check_login, async (req, res) => {
+  res.send("Hello " + req.username);
+});
+
+module.exports = router;
