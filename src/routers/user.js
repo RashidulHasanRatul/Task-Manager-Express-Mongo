d614741d1614b699266696d8c9c1fe0a30152b37
Rashidul Hasan <rhratul12@gmail.com>
2022-09-28 02:04:44 +0600

    added file validation

--- src/routers/user.js
+++ src/routers/user.js
@@ -29,6 +29,15 @@ router.post("/users", async (req, res) => {
 
 const upload = multer({
   dest: "./avatars",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload an image"));
+    }
+    cb(undefined, true);
+  },
 });
 
 router.post(
