403dd17eb9253c19741df5802e9dd4a1ba5b6af8
Rashidul Hasan <rhratul12@gmail.com>
2022-10-02 01:04:11 +0600

    Read url from env variable

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
+require("dotenv").config({ path: ".env" });
 const mongoose = require("mongoose");
-const url = "mongodb://127.0.0.1:27017/task-manager-app";
+const url = process.env.MONGODB_URL;
 
 mongoose.connect(url, {
   useNewUrlParser: true,
